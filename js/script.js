c23140daddd3ab704bcab9d1fa58079146bcd5c7
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-05-03 00:23:08 +0200

    added to top button

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const toTopBtn = document.querySelector('#to-top');
+
+function userScroll() {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      toTopBtn.classList.add('show');
+    } else {
+      toTopBtn.classList.remove('show');
+    }
+  });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function addBounce() {
+  document.querySelector('#chevron-up').classList.add('fa-bounce');
+}
+
+function removeBounce() {
+  document.querySelector('#chevron-up').classList.remove('fa-bounce');
+}
+
+toTopBtn.addEventListener('click', scrollToTop);
+toTopBtn.addEventListener('mouseover', addBounce);
+toTopBtn.addEventListener('mouseout', removeBounce);
+document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('load', userScroll);
