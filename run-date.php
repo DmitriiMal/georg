2276ae5c2bc91c286b5fe9afb1e669455f7d95c0
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-05-14 23:01:56 +0200

    Access-Control-Allow-Origin: *

--- run-date.php
+++ run-date.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Origin: https://lauftreff.netlify.app");
+header("Access-Control-Allow-Origin: *");
 
 echo "13. Mai 2024, 18.30 Uhr";
