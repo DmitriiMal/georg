948bb0cd88fa926cf36273e8ef8c1bccdc9a9c44
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-09-16 23:47:36 +0200

    removed db connection from run-date.php and set-date.php; added mysqli_close() to them; cleaned comments

--- run-date.php
+++ run-date.php
@@ -1,31 +1,6 @@
 <?php
 
-// Allowed Origins
-$allowedOrigins = array(
-  'http://127.0.0.1:5500',
-  'http://localhost:3000/run-date.php',
-  'http://localhost/georg/run-date.php',
-  'https://lauftreff.netlify.app',
-  'https://jugendlauftreff-wien.at'
-);
-
-$origin = $_SERVER['HTTP_ORIGIN'];
-
-if (in_array($origin, $allowedOrigins)) {
-  header("Access-Control-Allow-Origin: " . $origin);
-}
-
-// Database Connection
-$host = "localhost";
-$user = "nffc8c5_dmal_not_del";
-$pass = "JwgSmw3@kff6zcY";
-$dbName = "nffc8c5_jugendlauftreff";
-
-$connect = mysqli_connect($host, $user, $pass, $dbName);
-
-if (!$connect) {
-  echo "No connection";
-}
+require_once("db_connection.php");
 
 
 $sql = "SELECT * FROM run_date WHERE id=(
@@ -37,20 +12,10 @@ $row = mysqli_fetch_assoc($result);
 
 // Ensure that a row was fetched
 if ($row) {
-  // // Format the date and time
-  // $dateString = date("d. F Y", strtotime($row['DATE']));
-  // $timeString = date("H.i", strtotime($row['DATE']));
 
-  // Output the formatted date and time
-  // echo $dateString . ', ' . $timeString . ' Uhr';
   echo $row['DATE'];
 } else {
   // Handle case where no data is retrieved
   echo "No data found!";
 }
-
-
-
-// TO DO
-// The date should be converted to this format
-// echo "13. Mai 2024, 18.30 Uhr";
+mysqli_close($connect);
--- set-date.php
+++ set-date.php
@@ -1,33 +1,9 @@
 <?php
 
-// Allowed Origins
-$allowedOrigins = array(
-  'http://127.0.0.1:5500',
-  'http://localhost:3000/run-date.php',
-  'http://localhost/georg/run-date.php',
-  'https://lauftreff.netlify.app',
-  'https://jugendlauftreff-wien.at'
-);
-
-$origin = $_SERVER['HTTP_ORIGIN'];
-
-if (in_array($origin, $allowedOrigins)) {
-  header("Access-Control-Allow-Origin: " . $origin);
-}
-
-// Database Connection
-$host = "localhost";
-$user = "nffc8c5_dmal_not_del";
-$pass = "JwgSmw3@kff6zcY";
-$dbName = "nffc8c5_jugendlauftreff";
-
-$connect = mysqli_connect($host, $user, $pass, $dbName);
-
-if (!$connect) {
-  echo "No connection";
-}
+require_once("db_connection.php");
 
 if (isset($_POST['datetime'])) {
+
   $datetime = mysqli_real_escape_string($connect, $_POST['datetime']);
 
   $querry = "INSERT INTO `run_date`(`DATE`) VALUES ('$datetime')";
@@ -40,6 +16,4 @@ if (isset($_POST['datetime'])) {
 } else {
   echo 'no request';
 }
-
-
-// var_dump($datetime);
+mysqli_close($connect);
